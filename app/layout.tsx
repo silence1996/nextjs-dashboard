3437d4e075ad8c1be25b227da0d5bd31aaf88a8c
silence1996 <1042201782@qq.com>
2024-06-11 15:37:42 +0800

    添加元数据，进行SEO优化

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,20 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  // title: 'NextJs练习',
+  title: {
+    template: '%s | NextJs',
+    default: 'NextJs练习',
+  },
+  description: '一个简单的NextJs练习',
+  keywords: ['nextjs', 'typescript', 'react'],
+  openGraph: {
+    images: '/logo.png',
+  },
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'), // 用处未知
+};
 
 export default function RootLayout({
   children,
